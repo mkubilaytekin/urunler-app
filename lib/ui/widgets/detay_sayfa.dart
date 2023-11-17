b8682812180d2ff7a3a58bce6c69369491982626
mkubilaytekin <mkubilaytekin@gmail.com>
2023-11-17 10:18:18 +0300

    product listing ui for e-commerce

---
+++ lib/ui/widgets/detay_sayfa.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:urunler_app/data/entity/urunler.dart';
+
+class DetaySayfa extends StatefulWidget {
+  Urunler urun;
+
+  DetaySayfa({super.key, required this.urun});
+
+  @override
+  State<DetaySayfa> createState() => _DetaySayfaState();
+}
+
+class _DetaySayfaState extends State<DetaySayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.urun.ad),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset("resimler/${widget.urun.resim}"),
+            Text(
+              "${widget.urun.fiyat.toString()} ₺",
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
